4f84cc0979369a85d6c0fe84c9746ea278bb21fc
Michael Agarkov <michaelagarkov@outlook.com>
2021-07-12 15:31:54 +0300

    Add files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -4,17 +4,11 @@ interface
 
 uses System, System.Drawing, System.Windows.Forms, About;
 
-var
-  SavedToDo, SavedForLater, SavedIdeas: string;
-
 type
   Form1 = class(Form)
     procedure Form1_Load(sender: Object; e: EventArgs);
     procedure treeView1_AfterSelect(sender: Object; e: TreeViewEventArgs);
-    procedure Panel2_SizeChanged(sender: Object; e: EventArgs);
     procedure textBox1_TextChanged(sender: Object; e: EventArgs);
-    procedure button1_Click(sender: Object; e: EventArgs);
-    procedure button2_Click(sender: Object; e: EventArgs);
     procedure aboutToolStripMenuItem_Click(sender: Object; e: EventArgs);
     procedure toDoToolStripMenuItem_Click(sender: Object; e: EventArgs);
     procedure forLaterToolStripMenuItem_Click(sender: Object; e: EventArgs);
@@ -26,12 +20,7 @@ type
     {$resource Unit1.Form1.resources}
     menuStrip1: MenuStrip;
     taskVaultToolStripMenuItem: ToolStripMenuItem;
-    button1: Button;
-    treeView1: TreeView;
-    button2: Button;
     splitContainer2: SplitContainer;
-    splitContainer1: SplitContainer;
-    splitContainer3: SplitContainer;
     toDoToolStripMenuItem: ToolStripMenuItem;
     forLaterToolStripMenuItem: ToolStripMenuItem;
     ideasToolStripMenuItem: ToolStripMenuItem;
@@ -40,9 +29,11 @@ type
     toolStripSeparator1: ToolStripSeparator;
     fontToolStripMenuItem: ToolStripMenuItem;
     fontDialog1: FontDialog;
-    textBox1: TextBox;
     contextMenuStrip1: System.Windows.Forms.ContextMenuStrip;
     fontToolStripMenuItem1: ToolStripMenuItem;
+    textBox1: TextBox;
+    treeView1: TreeView;
+    panel1: Panel;
     aboutToolStripMenuItem: ToolStripMenuItem;
     {$include Unit1.Form1.inc}
   {$endregion FormDesigner}
@@ -57,7 +48,30 @@ implementation
 
 procedure Form1.Form1_Load(sender: Object; e: EventArgs);
 begin
-  
+  if(treeView1.SelectedNode = treeView1.Nodes[0]) then begin
+    try
+      var ToDoFile := new System.IO.StreamReader('ToDo.txt', System.Text.Encoding.Default);
+      textBox1.Text := ToDoFile.ReadToEnd;
+      ToDoFile.Close;
+    except
+    end;
+  end;
+  if(treeView1.SelectedNode = treeView1.Nodes[1]) then begin
+    try
+      var ForLaterFile := new System.IO.StreamReader('ForLater.txt', System.Text.Encoding.Default);
+      textBox1.Text := ForLaterFile.ReadToEnd;
+      ForLaterFile.Close;
+    except
+    end;
+  end;
+  if(treeView1.SelectedNode = treeView1.Nodes[2]) then begin
+    try
+      var IdeasFile := new System.IO.StreamReader('Ideas.txt', System.Text.Encoding.Default);
+      textBox1.Text := IdeasFile.ReadToEnd;
+      IdeasFile.Close;
+    except
+    end;
+  end;
 end;
 
 procedure Form1.treeView1_AfterSelect(sender: Object; e: TreeViewEventArgs);
@@ -68,7 +82,7 @@ begin
       textBox1.Text := ToDoFile.ReadToEnd;
       ToDoFile.Close;
     except
-      textBox1.Text := SavedToDo;
+      textBox1.Text := '';
     end;
   end;
   if(treeView1.SelectedNode = treeView1.Nodes[1]) then begin
@@ -77,7 +91,7 @@ begin
       textBox1.Text := ForLaterFile.ReadToEnd;
       ForLaterFile.Close;
     except
-      textBox1.Text := SavedForLater;
+      textBox1.Text := '';
     end;
   end;
   if(treeView1.SelectedNode = treeView1.Nodes[2]) then begin
@@ -86,68 +100,30 @@ begin
       textBox1.Text := IdeasFile.ReadToEnd;
       IdeasFile.Close;
     except
-      textBox1.Text := SavedIdeas;
+      textBox1.Text := '';
     end;
   end;
-end;
-
-procedure Form1.Panel2_SizeChanged(sender: Object; e: EventArgs);
-begin
-  button1.Size := System.Drawing.Size.Create(splitContainer1.SplitterDistance - 24, 23);
-  button2.Size := System.Drawing.Size.Create(splitContainer1.SplitterDistance - 24, 23);
 end;
 
 procedure Form1.textBox1_TextChanged(sender: Object; e: EventArgs);
 begin
-  if(treeView1.SelectedNode = treeView1.Nodes[0]) then SavedToDo := textBox1.Text;
-  if(treeView1.SelectedNode = treeView1.Nodes[1]) then SavedForLater := textBox1.Text;
-  if(treeView1.SelectedNode = treeView1.Nodes[2]) then SavedIdeas := textBox1.Text;
-end;
-
-procedure Form1.button1_Click(sender: Object; e: EventArgs);
-begin
-  if(treeView1.SelectedNode = treeView1.Nodes[0]) then begin
-    var ToDoFile := new System.IO.StreamWriter('ToDo.txt', false, System.Text.Encoding.Default);
-    ToDoFile.Write(textBox1.Text);
-    ToDoFile.Close;
-  end;
-  if(treeView1.SelectedNode = treeView1.Nodes[1]) then begin
-    var ForLaterFile := new System.IO.StreamWriter('ForLater.txt', false, System.Text.Encoding.Default);
-    ForLaterFile.Write(textBox1.Text);
-    ForLaterFile.Close;
-  end;
-  if(treeView1.SelectedNode = treeView1.Nodes[2]) then begin
-    var IdeasFile := new System.IO.StreamWriter('Ideas.txt', false, System.Text.Encoding.Default);
-    IdeasFile.Write(textBox1.Text);
-    IdeasFile.Close;
-  end;
-end;
-
-procedure Form1.button2_Click(sender: Object; e: EventArgs);
-begin
-  if(treeView1.SelectedNode = treeView1.Nodes[0]) then begin
-    try
-      var ToDoFile := new System.IO.StreamReader('ToDo.txt', System.Text.Encoding.Default);
-      textBox1.Text := ToDoFile.ReadToEnd;
+  try
+    if(treeView1.SelectedNode = treeView1.Nodes[0]) then begin
+      var ToDoFile := new System.IO.StreamWriter('ToDo.txt', false, System.Text.Encoding.Default);
+      ToDoFile.Write(textBox1.Text);
       ToDoFile.Close;
-    except
     end;
-  end;
-  if(treeView1.SelectedNode = treeView1.Nodes[1]) then begin
-    try
-      var ForLaterFile := new System.IO.StreamReader('ForLater.txt', System.Text.Encoding.Default);
-      textBox1.Text := ForLaterFile.ReadToEnd;
+    if(treeView1.SelectedNode = treeView1.Nodes[1]) then begin
+      var ForLaterFile := new System.IO.StreamWriter('ForLater.txt', false, System.Text.Encoding.Default);
+      ForLaterFile.Write(textBox1.Text);
       ForLaterFile.Close;
-    except
     end;
-  end;
-  if(treeView1.SelectedNode = treeView1.Nodes[2]) then begin
-    try
-      var IdeasFile := new System.IO.StreamReader('Ideas.txt', System.Text.Encoding.Default);
-      textBox1.Text := IdeasFile.ReadToEnd;
+    if(treeView1.SelectedNode = treeView1.Nodes[2]) then begin
+      var IdeasFile := new System.IO.StreamWriter('Ideas.txt', false, System.Text.Encoding.Default);
+      IdeasFile.Write(textBox1.Text);
       IdeasFile.Close;
-    except
     end;
+  except
   end;
 end;
 
